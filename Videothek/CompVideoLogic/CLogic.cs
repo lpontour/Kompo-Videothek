331ea3741107b00f987a15d3691ebaf52a211643
Lukas Paulmann <l.paulmann@ostfalia.de>
2018-08-13 15:41:22 +0200

    CLogic abgeschlossen

--- Videothek/CompVideoLogic/CLogic.cs
+++ Videothek/CompVideoLogic/CLogic.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace VideoLogic
 {
-    internal class CLogic
+    internal class CLogic : ILogic
     {
+		#region fields
+		private IData _data;
+		#endregion
+
+		#region interface properties         
+		public ILogicSearch Search {get; internal set;}
+		public ILogicLoan Loan {get; internal set;}
+		#endregion
+
+		#region ctor
+		internal CLogic(IData data) 
+		{
+			_data = data;
+		}
+		#endregion
+
+		#region interface ILogic methods
+		 // 2. Initialisierung der Anwendung
+		 // Anzahl Autos in der DB ermitteln(=nCars)    
+		// Liste der Hersteller (=makers) ermitteln, als object Array
+		public void InitApp(ref int nVideos, out object[] arrayMakers) 
+		{
+			_data.InitApp(ref nVideos, out IList<string> makers);
+
+			if(makers == null)
+			{
+				throw new CDataException("Produzentenliste ist leer");
+			}
+            
+			arrayMakers = Util.ToArray(makers);
+		}      
+		#endregion
     }
 }
