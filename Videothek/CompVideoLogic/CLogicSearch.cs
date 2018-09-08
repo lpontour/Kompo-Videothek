5565ffeae9977c3b6ecb7359e289c53513929a92
Lukas Paulmann <l.paulmann@ostfalia.de>
2018-09-08 13:15:44 +0200

    Anpassen der Read-Methoden

--- Videothek/CompVideoLogic/CLogicSearch.cs
+++ Videothek/CompVideoLogic/CLogicSearch.cs
@@ -39,84 +39,84 @@ namespace VideoLogic
 
 		}
 
-		public object[] ReadID(int ID)
+		public object[] ReadID(string value)
 		{
 			IList<int> listID = null;
-			_dataSearch.ReadID(ID, out listID);
+			_dataSearch.ReadID(value, out listID);
 			if (listID == null)
 				throw new CDataException("ID-Liste ist leer");
 			return Util.ToArray(listID);
 		}
 
-		public object[] ReadTitle(string Title)
+		public object[] ReadTitle(string value)
 		{
 			IList<string> listTitle = null;
-			_dataSearch.ReadTitle(Title, out listTitle);
+			_dataSearch.ReadTitle(value, out listTitle);
 			if (listTitle == null)
 				throw new CDataException("Titelliste ist leer");
 			return Util.ToArray(listTitle);
 		}
 
-		public object[] ReadGenre(string Genre)
+		public object[] ReadGenre(string value)
 		{
 			IList<string> listGenre = null;
-			_dataSearch.ReadGenre(Genre, out listGenre);
+			_dataSearch.ReadGenre(value, out listGenre);
 			if (listGenre == null)
 				throw new CDataException("Genreliste ist leer");
 			return Util.ToArray(listGenre);
 		}
 
-		public object[] ReadBorrowingrate(double BorrowingRate)
+		public object[] ReadBorrowingrate(string value)
 		{
 			IList<double> listBorrowingRate = null;
-			_dataSearch.ReadBorrowingRate(BorrowingRate, out listBorrowingRate);
+			_dataSearch.ReadBorrowingRate(value, out listBorrowingRate);
 			if (listBorrowingRate == null)
 				throw new CDataException("Preisiste ist leer");
 			return Util.ToArray(listBorrowingRate);
 		}
 
-		public object[] ReadReleaseYear(int ReleaseYear)
+		public object[] ReadReleaseYear(string value)
 		{
 			IList<int> listReleaseYear = null;
-			_dataSearch.ReadReleaseYear(ReleaseYear, out listReleaseYear);
+			_dataSearch.ReadReleaseYear(value, out listReleaseYear);
 			if (listReleaseYear == null)
 				throw new CDataException("Erscheinungsjahrliste ist leer");
 			return Util.ToArray(listReleaseYear);
 		}
 
-		public object[] ReadRunningTime(int RunningTime)
+		public object[] ReadRunningTime(string value)
 		{
 			IList<int> listRunningTime = null;
-			_dataSearch.ReadRunningTime(RunningTime, out listRunningTime);
+			_dataSearch.ReadRunningTime(value, out listRunningTime);
 			if (listRunningTime == null)
 				throw new CDataException("Laufzeitliste ist leer");
 			return Util.ToArray(listRunningTime);
 		}
 
-		public object[] ReadRated(int Rated)
+		public object[] ReadRated(string value)
 		{
 			IList<int> listRated = null;
-			_dataSearch.ReadRated(Rated, out listRated);
+			_dataSearch.ReadRated(value, out listRated);
 			if (listRated == null)
 				throw new CDataException("FSK-Liste ist leer");
 			return Util.ToArray(listRated);
 		}
 
-		public object[] ReadBorrower(string Borrower)
+		public object[] ReadBorrower(string value)
 		{
 			IList<string> listBorrower = null;
-			_dataSearch.ReadBorrower(Borrower, out listBorrower);
-			if (listBorrower == null)
-				throw new CDataException("Ausleiherliste ist leer");
+			_dataSearch.ReadBorrower(value, out listBorrower);
+			//if (listBorrower == null)
+				//throw new CDataException("Ausleiherliste ist leer");
 			return Util.ToArray(listBorrower);
 		}
 
-		public object[] ReadReturnDate(DateTime ReturnDate)
+		public object[] ReadReturnDate(string value)
 		{
 			IList<DateTime> listReturnDate = null;
-			_dataSearch.ReadReturnDate(ReturnDate, out listReturnDate);
-			if (listReturnDate == null)
-				throw new CDataException("Rückgabedatumliste ist leer");
+			_dataSearch.ReadReturnDate(value, out listReturnDate);
+			//if (listReturnDate == null)
+				//throw new CDataException("Rückgabedatumliste ist leer");
 			return Util.ToArray(listReturnDate);
 		}
 
