ca7e4fadd7591ab7bc1b832326e280f08906aebc
Lukas Paulmann <l.paulmann@ostfalia.de>
2018-09-08 15:34:07 +0200

    Ändern der Datentypen

--- Videothek/CompVideoLogic/IDataSearch.cs
+++ Videothek/CompVideoLogic/IDataSearch.cs
@@ -7,15 +7,15 @@ namespace VideoLogic
 {
     public interface IDataSearch
     {
-		void ReadID(int ID, out IList<int> ListID);
+		void ReadID(string ID, out IList<int> ListID);
 		void ReadTitle(string Title, out IList<string> ListTitle);
 		void ReadGenre(string Genre, out IList<string> ListGenre);
-		void ReadBorrowingRate(double BorrowingRate, out IList<double> ListBorrowingRate);
-		void ReadReleaseYear(int ReleaseYear, out IList<int> ListReleaseYear);
-		void ReadRunningTime(int RunningTime, out IList<int> ListRunningTime);
-		void ReadRated(int Rated, out IList<int> ListRated);
+		void ReadBorrowingRate(string BorrowingRate, out IList<double> ListBorrowingRate);
+		void ReadReleaseYear(string ReleaseYear, out IList<int> ListReleaseYear);
+		void ReadRunningTime(string RunningTime, out IList<int> ListRunningTime);
+		void ReadRated(string Rated, out IList<int> ListRated);
 		void ReadBorrower(string Borrower, out IList<string> ListBorrower);
-		void ReadReturnDate(DateTime ReturnDate, out IList<DateTime> ListReturnDate);
+		void ReadReturnDate(string ReturnDate, out IList<DateTime> ListReturnDate);
 		void ReadVideos(VideoDtoSearch VideoDtoSelected, out IList<Loan> Videos);
 		void ReadVideos(VideoDtoSearch VideoDtoSelected, out DataTable dTableVideos);
     }
